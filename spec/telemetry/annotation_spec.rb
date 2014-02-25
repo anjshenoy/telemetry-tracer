08dd954848fc8b2a6cbc09190aa207568df3cae9
Anjali Shenoy <ashenoy@yammer-inc.com>
2014-02-25 07:55:31 -0800

    rspec'd annotation_test

--- spec/telemetry/annotation_spec.rb
+++ spec/telemetry/annotation_spec.rb
@@ -1,29 +1,27 @@
-require "test_helper"
+require "spec_helper"
 require "telemetry/annotation"
+require "pp"
 
 module Telemetry
   describe Annotation do
-
-    def setup
-      @a = Annotation.new(hash)
-    end
+    let(:a) { Annotation.new(hash) }
 
     it "stores the incoming list of key value pairs as is" do
-      assert_equal hash, @a.params
+      expect(a.params).to eq(hash)
     end
 
     it "logs the current time" do
-      assert @a.time < (Time.now.to_f * 1000000000).to_i
+      expect(a.time).to be < (Time.now.to_f * 1000000000).to_i
     end
 
     it "comes with a to_hash method which lists its internals" do
-      assert_equal hash["foo"], @a.to_hash["foo"]
-      assert_equal true, !@a.to_hash[:time].nil?
+      expect(a.to_hash["foo"]).to eq(hash["foo"])
+      expect(a.to_hash[:time]).not_to be_nil
     end
 
     it "logs the time to process if one is provided" do
       a = Annotation.new(hash, 1.123)
-      assert_equal 1.123, a.time_to_process
+      expect(a.time_to_process).to eq(1.123)
     end
 
     private
