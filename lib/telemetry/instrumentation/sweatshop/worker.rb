f2b17aec73d2ebfe5dce78cfc097bba459630a82
Anjali Shenoy <ashenoy@yammer-inc.com>
2014-05-05 10:11:37 -0700

    Added alias method chains around sweatshop enqueue, do_task methods so the calling source does not have fetch a trace directly from the Tracer

---
+++ lib/telemetry/instrumentation/sweatshop/worker.rb
@@ -0,0 +1,40 @@
+require "sweatshop"
+
+module Sweatshop
+  class Worker
+    class << self
+      def enqueue_with_trace(task, *args)
+        task[:args] << Telemetry::Tracer.current_trace_headers
+        enqueue_without_trace(task, *args)
+      end
+      alias_method :enqueue_without_trace, :enqueue
+      alias_method :enqueue, :enqueue_with_trace
+
+      def do_task_with_trace(task)
+        #Need to extract trace_headers from task[:args] even if the Tracer is turned off
+        #This is because we don't want the trace headers to propagate downstream
+        #to workers that process task[:args] - the workers should not have to know
+        #of any tracer bits appended to the args
+        trace_headers = trace_headers_exist?(task) ? task[:args].delete_at(-1) : {}
+
+        Telemetry::Tracer.fetch(trace_headers).apply(queue_name) do
+          do_task_without_trace(task)
+        end
+      end
+      alias_method :do_task_without_trace, :do_task
+      alias_method :do_task, :do_task_with_trace
+
+      def trace_headers_exist?(task)
+        possible_trace_headers = task[:args][-1]
+        if possible_trace_headers.is_a?(Hash) && 
+          (possible_trace_headers.has_key?(Telemetry::Tracer::TRACE_HEADER_KEY) || 
+           possible_trace_headers.has_key?(Telemetry::Tracer::SPAN_HEADER_KEY))
+          true
+        else
+          false
+        end
+      end
+    end
+
+  end
+end
---
+++ spec/telemetry/instrumentation/sweatshop/worker_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+require "telemetry/tracer"
+require "telemetry/instrumentation/sweatshop/worker"
+
+module Sweatshop
+  class TestWorker < Worker
+    def self.enqueue_without_trace(task, *args)
+      task[:args]
+    end
+
+    def self.do_task_without_trace(task)
+      task[:args]
+    end
+  end
+
+  describe TestWorker do
+    let(:args) { [{:foo => 1, :bar => "abc"}]}
+    let(:task)  { {:args=>  args,
+                   :method => "process_message",
+                   :uid => "ab425204",
+                   :queued_at => 1399253770.1600323
+    }}
+    let(:trace_headers) { {Telemetry::Tracer::TRACE_HEADER_KEY => 1233467890,
+                           Telemetry::Tracer::SPAN_HEADER_KEY  => 12345} }
+
+    it "appends the trace information to the outgoing task" do
+      tracer = Telemetry::Tracer.with_config(tracer_opts).fetch
+      tracer.apply("span1") do |trace|
+        result = TestWorker.enqueue(task)
+
+        trace_headers = result[-1]
+        expect(trace_headers).to include({Telemetry::Tracer::TRACE_HEADER_KEY => tracer.id})
+        expect(trace_headers).to include({Telemetry::Tracer::SPAN_HEADER_KEY => tracer.current_span_id})
+      end
+    end
+
+
+    it "strips the dequeued task of the tracer bits" do
+      task[:args] << trace_headers
+      result = TestWorker.do_task(task)
+      expect(result).to eq(args)
+    end
+
+    it "stays idempotent if the dequeued task does not carry tracer bits" do
+      result = TestWorker.do_task(task)
+      expect(result).to eq(args)
+    end
+
+    it "still strips the incoming dequeued task off tracer bits even if the tracer is switched off" do
+      Telemetry::Tracer.instance_variable_set(:@config, nil)
+      Telemetry::Tracer.config = tracer_opts.merge({"enabled" => false})
+      expect(Telemetry::Tracer.run?).to be_false
+
+      task[:args] << trace_headers
+      result = TestWorker.do_task(task)
+      expect(result).to eq(args)
+    end
+  end
+end
