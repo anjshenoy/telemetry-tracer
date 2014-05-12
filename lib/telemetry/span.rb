5140df9437c86f364c718cac2e46d9142e4918b3
Anjali Shenoy <ashenoy@yammer-inc.com>
2014-05-12 12:54:32 -0700

    Removed TODO

--- lib/telemetry/span.rb
+++ lib/telemetry/span.rb
@@ -29,7 +29,6 @@ module Telemetry
       @parent_span_id.nil?
     end
 
-    #TODO: raise Spanstopped exception if stopped
     def annotate(key, message)
       raise SpanStoppedException if stopped?
       @annotations << Annotation.new({key => message})
