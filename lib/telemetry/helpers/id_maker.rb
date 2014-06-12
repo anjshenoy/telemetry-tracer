8db507ac96e087ac551ebc70ed63b4b3c88f1fd5
Anjali Shenoy <ashenoy@yammer-inc.com>
2014-06-11 17:37:24 -0700

    Constantized upper bound numbers during ID generation there's no need to calculate them each time

--- lib/telemetry/helpers/id_maker.rb
+++ lib/telemetry/helpers/id_maker.rb
@@ -1,9 +1,14 @@
 module Telemetry
   module Helpers
+
     module IdMaker
+      TWO_POWER_THIRTY_TWO = 2**32
+      TWO_POWER_SIXTY_FOUR = 2**64
+
       srand
+
       def generate_id
-        (self.class == Span) ? rand(1..2**32) : rand(1..2**64)
+        (self.class == Span) ? rand(1..TWO_POWER_THIRTY_TWO) : rand(1..TWO_POWER_SIXTY_FOUR)
       end
     end
   end
